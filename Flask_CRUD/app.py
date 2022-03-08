f4bfe7a19f10c3e55b66f87680f6cd7b1f1dc08c
1000007201 <nishant.sharma5504@gmail.com>
2022-03-08 23:42:01 +0530

    [Nishant] Add:CRUD operation is done by flask-restful

--- Flask_CRUD/app.py
+++ Flask_CRUD/app.py
@@ -1,52 +1,50 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from flask_restful import Api, Resource
 
 
 app = Flask(__name__)
-
+api = Api(app)
 # connecting with Database
 app.config["MONGO_URI"] = "mongodb://localhost:27017/address_book"
 mongo_client = PyMongo(app)
 db = mongo_client.db
 
 
-@app.route('/add', methods=['POST'])
-def add_value():
-    result = {
-        'Name': request.form['Name'],
-        'PhoneNumber': request.form['PhoneNumber']
-    }
-    db.users.insert_one(result)
-    return jsonify(message='Success')
+class Crud(Resource):
+    def get(self):
+        data = list(db.users.find())
+        for itr in data:
+            itr["_id"] = str(itr["_id"])
+        return jsonify(data)
+
+    def post(self):
+        result = {
+            'Name': request.form['Name'],
+            'PhoneNumber': request.form['PhoneNumber']
+        }
+        db.users.insert_one(result)
+        return jsonify(message='Success')
 
 
-@app.route('/get/allUsers', methods=['GET'])
-def get_users():
-    data = list(db.users.find())
-    for itr in data:
-        itr["_id"] = str(itr["_id"])
-    return jsonify(data)
+class CrudName(Resource):
+    def get(self, name):
+        data = list(db.users.find({"Name":name}))
+        for itr in data:
+            result = {"Name": itr["Name"], "PhoneNumber": itr["PhoneNumber"]}
+        return jsonify(result)
+
+    def patch(self, name):
+            db.users.update_one({"Name": name}, {"$set": {"PhoneNumber": request.form['PhoneNumber']}})
+            return jsonify(message="User Updated")
+
+    def delete(self, name):
+            db.users.delete_one({"Name": name})
+            return jsonify(message="Deleted")
 
 
-@app.route('/get/<name>', methods=['GET'])
-def get_name(name):
-    data = list(db.users.find({'Name':name}))
-    for itr in data:
-        result = { "Name": itr["Name"], "PhoneNumber": itr["PhoneNumber"]}
-
-    return jsonify(result)
-
-
-@app.route('/update/<name>', methods=['PATCH'])
-def update(name):
-    db.users.update_one({"Name":name}, {"$set":{"PhoneNumber":request.form['PhoneNumber']}})
-    return jsonify(message="User Updated")
-
-
-@app.route('/delete/<name>', methods=['DELETE'])
-def delete(name):
-    db.users.delete_one({"Name":name})
-    return jsonify(message="Deleted")
+api.add_resource(Crud, '/get')
+api.add_resource(CrudName, '/get/<name>')
 
 
 if __name__ == "__main__":
